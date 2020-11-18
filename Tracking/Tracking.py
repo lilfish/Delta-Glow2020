6e9cc4e8b98339dd080804ee432de3caa37c09a7
Bouman,Dave D.H.M <d.reijnen@student.fontys.nl>
2020-11-18 10:22:35 +0100

    Added upperbody tracking
    Added upperbody tracking

--- Tracking/Tracking.py
+++ Tracking/Tracking.py
@@ -4,9 +4,9 @@ import numpy as np
 import imutils
 import cv2
 
-
 capture = cv2.VideoCapture(0)
-bodyCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+bodyCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_upperbody.xml')
+
 
 def find_marker(image):
     # convert the image to grayscale, blur it, and detect edges
@@ -21,50 +21,67 @@ def find_marker(image):
     return cv2.minAreaRect(c)
 
 
-def distance_to_camera(image,knownWidth, focalLength, perWidth):
+def distance_to_camera(image, knownWidth, focalLength, perWidth):
     # compute and return the distance from the maker to the camera
     return (knownWidth * focalLength) / perWidth
 
 
+def body_tracking():
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    cap = capture
+    while True:
+        _, frame = cap.read()
+        found, w = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.05)
+        draw_detections(frame, found)
+        cv2.imshow('feed', frame)   
+        ch = 0xFF & cv2.waitKey(1)
+        if ch == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+def inside(r, q):
+    rx, ry, rw, rh = r
+    qx, qy, qw, qh = q
+    return rx > qx and ry > qy and rx + rw < qx + qw and ry + rh < qy + qh
+
+
+def draw_detections(img, rects, thickness=1):
+    for x, y, w, h in rects:
+        # the HOG detector returns slightly larger rectangles than the real objects.
+        # so we slightly shrink the rectangles to get a nicer output.
+        pad_w, pad_h = int(0.15 * w), int(0.05 * h)
+        cv2.rectangle(img, (x + pad_w, y + pad_h), (x + w - pad_w, y + h - pad_h), (0, 255, 0), thickness)
+
+
+body_tracking()
+
 bodyDetector = bodyCascade
 count = 0
-while(True):
-    # Capture frame-by-frame
-    ret, frame = capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    people = bodyDetector.detectMultiScale(gray, 1.3, 5)
-
-    for (x, y, w, h) in people:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        count += 1
-        gray = cv2.resize(gray[y:y + h, x:x + w], (128, 128), interpolation=cv2.INTER_AREA)
-
-    # Our operations on the frame come here
-    KNOWN_DISTANCE = 30
-    KNOWN_WIDTH = 30
-    marker = find_marker(frame)
-    focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
-    centimetre = distance_to_camera(frame,KNOWN_WIDTH, focalLength, marker[1][0])
-    # draw a bounding box around the image and display it
-    box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-    box = np.int0(box)
-    cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
-    cv2.putText(
-        frame,
-        "%.2fft" % (centimetre / 12),
-        (frame.shape[1] - 200, frame.shape[0] - 20),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        2.0,
-        (0, 255, 0),
-        3,
-    )
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+while (True):
+    # # Our operations on the frame come here
+    # KNOWN_DISTANCE = 30
+    # KNOWN_WIDTH = 30
+    # marker = find_marker(frame)
+    # focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+    # centimetre = distance_to_camera(frame,KNOWN_WIDTH, focalLength, marker[1][0])
+    # # draw a bounding box around the image and display it
+    # box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+    # box = np.int0(box)
+    # cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
+    # cv2.putText(
+    #     frame,
+    #     "%.2fft" % (centimetre / 12),
+    #     (frame.shape[1] - 200, frame.shape[0] - 20),
+    #     cv2.FONT_HERSHEY_SIMPLEX,
+    #     2.0,
+    #     (0, 255, 0),
+    #     3,
+    # )
+    # # Display the resulting frame
+    # cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 capture.release()
 cv2.destroyAllWindows()
-
-
-
